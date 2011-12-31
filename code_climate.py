694703f77dad2da98ce0f6fa13eacb0372d35d0d
Noah Davis <noahd1@yahoo.com>
2011-12-31 15:09:15 -0500

    don't bubble up GitFileInfo errors; print to standard err

--- code_climate.py
+++ code_climate.py
@@ -1,14 +1,18 @@
 import os
+import sys
 import urllib
 import webbrowser
-import git_utils
 import sublime, sublime_plugin
+import git_utils
 
 class CodeClimateCommand(sublime_plugin.TextCommand):
 
   def run(self, args):
-    file_info = git_utils.GitFileInfo(self.view.file_name())
-    params = urllib.urlencode({"repo": file_info.remote_repository(), "path": file_info.path()})
-    url = "https://codeclimate.com/browse?%s" % params
-    print "Opening URL: " + url
-    webbrowser.open_new(url)
+    try:
+      file_info = git_utils.GitFileInfo(self.view.file_name())
+      params = urllib.urlencode({"repo": file_info.remote_repository(), "path": file_info.path()})
+      url = "https://codeclimate.com/browse?%s" % params
+      print "Opening URL: " + url
+      webbrowser.open_new(url)
+    except git_utils.GitInfoError as err:
+      sys.stderr.write("Cannot open file in CodeClimate: " + str(err))
